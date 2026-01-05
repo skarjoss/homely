d91090809e3d2a3db943d779b9f1b4b8c417f942
Oscar Quinde <oscarquinde@live.com>
2026-01-04 21:36:15 -0500

    feat: Add alias setup in init script and update README for terminal reload instructions
    Updated install instructions

--- init.sh
+++ init.sh
@@ -2,4 +2,39 @@
 
 cp -i Homely.sample.yaml Homely.yaml
 
+ALIAS_LINE='alias homely="sudo ruby ~/homely/Homelyfile.rb"'
+BASHRC="$HOME/.bashrc"
+SOURCED=0
+
+if [ "${BASH_SOURCE[0]}" != "$0" ]; then
+  SOURCED=1
+fi
+
+if [ -f "$BASHRC" ]; then
+  if ! grep -Fq "$ALIAS_LINE" "$BASHRC"; then
+    printf '\n%s\n' "$ALIAS_LINE" >> "$BASHRC"
+    if [ "$SOURCED" -eq 1 ]; then
+      source "$BASHRC"
+      echo "Alias homely added and loaded from ~/.bashrc"
+    else
+      echo "Alias homely added to ~/.bashrc. Open a new shell or run: source ~/.bashrc"
+    fi
+  else
+    if [ "$SOURCED" -eq 1 ]; then
+      source "$BASHRC"
+      echo "Alias homely already present and loaded from ~/.bashrc"
+    else
+      echo "Alias homely already present in ~/.bashrc"
+    fi
+  fi
+else
+  printf '%s\n' "$ALIAS_LINE" > "$BASHRC"
+  if [ "$SOURCED" -eq 1 ]; then
+    source "$BASHRC"
+    echo "Created ~/.bashrc and loaded homely alias"
+  else
+    echo "Created ~/.bashrc and added homely alias. Open a new shell or run: source ~/.bashrc"
+  fi
+fi
+
 echo "Homely initialized!"
